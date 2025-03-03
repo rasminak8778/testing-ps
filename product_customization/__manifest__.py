7f3cf109ab1aac8696b76f80fe24a98198bc931c
CybroOdooDev <cybroodoo@gmail.com>
2025-03-03 13:51:11 +0530

    [ADD] Initia;l commit

---
+++ product_customization/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Product Customization',
+    'version': '17.0.1.0.0',
+    'summary': """This module allows to import products based on Uom""",
+    'author': 'Harrison Consulting',
+    'website': 'https://www.harrison.consulting',
+    'sequence': 0,
+    'license': 'GPL-3',
+    'description': """  """,
+    'category': '',
+    'depends': ['product', 'sale'],
+    'data': [
+        'views/product_template_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+}
+
+
