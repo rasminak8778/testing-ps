199ba109e63133cdf5239fa2d7b0268a1102d217
rasminak8778 <165371733+rasminak8778@users.noreply.github.com>
2025-03-03 16:25:04 +0530

    Delete product_customization directory

--- product_customization/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import product_template
-from . import purchase_order_line
--- product_customization/models/product_template.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import fields, models, api
-
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    case_per_pallet = fields.Char(string="Cases Per Pallet", help="This is the cases per pallet")
