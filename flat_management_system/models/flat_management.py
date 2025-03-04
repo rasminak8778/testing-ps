9ef1e60c2126b69865f0c76d2dbaf768146eca08
CybroOdooDev <cybroodoo@gmail.com>
2025-03-04 16:48:13 +0530

    [UPDT]update fieds

--- flat_management_system/models/flat_management.py
+++ flat_management_system/models/flat_management.py
@@ -10,4 +10,4 @@ class FlatManagement(models.Model):
     user_id = fields.Many2one('res.partner',string='Customer', required= True)
     flat_manage_ids = fields.One2many('flat.flats', 'flat_id', string='Flat details')
 
-
+	
