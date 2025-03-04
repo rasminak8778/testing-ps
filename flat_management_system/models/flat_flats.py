3578be491cba6bab202f1f7f78eadacef53fa6f8
CybroOdooDev <cybroodoo@gmail.com>
2025-03-04 15:51:25 +0530

    [ADD] Initial commit

--- flat_management_system/models/flat_flats.py
+++ flat_management_system/models/flat_flats.py
@@ -8,7 +8,13 @@ class FlatFlats(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _rec_name = 'name'
 
-    name = fields.Char('Nameqqq', required=True)
+    name = fields.Char('name', required=True)
     description = fields.Char(string='Description', help='Description for the flat')
     amount = fields.Float(string='Amount')
     flat_id = fields.Many2one('flat.management', string="Flat")
+    
+    sum= fields.Float(string='Sum')
+    location = fields.Char(string='Location', help='Flat location')
+    size = fields.Float(string='Size (sq ft)', help='Flat size in square feet')
+    num_bedrooms = fields.Integer(string='Number of Bedrooms', help='Total bedrooms in the flat')
+    num_bathrooms = fields.Integer(string='Number of Bathrooms', help='Total bathrooms in the flat')
